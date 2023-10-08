5a136a519d0392b5825ca666a939c1e1436537dd
Stefano Bisio <s.b.milano@gmail.com>
2023-10-08 15:55:32 +0100

    show/hide scroll to top button when scrolling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
   import "../app.css";
-  import Header from '../components/Header.svelte';
-  import Footer from '../components/Footer.svelte';
+  import Header from "../components/Header.svelte";
+  import Footer from "../components/Footer.svelte";
 
   let scrollY = 0;
   let innerHeight = 0;
@@ -13,14 +13,19 @@
       behavior: "smooth",
     });
   }
-
 </script>
 
-<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
-  <div class="">
+<div
+  class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
+>
+  <div
+    class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " +
+      (scrollY > 0
+        ? "opacity-full pointer-events-auto"
+        : "opacity-0 pointer-events-none")}
+  >
     <button>
-      <i class="fa-solid fa-arrow-up" on:click={goTop}
-      ></i>
+      <i class="fa-solid fa-arrow-up" on:click={goTop} />
     </button>
   </div>
 
@@ -31,4 +36,4 @@
   <Footer />
 </div>
 
-<svelte:window bind:scrollY={scrollY} bind:innerHeight bind:innerWidth />
+<svelte:window bind:scrollY bind:innerHeight bind:innerWidth />
