04fa676de90871b5808517cd845f68ef4c206a4c
Stefano Bisio <s.b.milano@gmail.com>
2023-10-08 15:38:31 +0100

    add main, header, footer components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
-    import "../app.css";
-  </script>
-  
+  import "../app.css";
+  import Header from '../components/Header.svelte';
+  import Footer from '../components/Footer.svelte';
+
+</script>
+
+<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+  <div class="">
+    <button>
+      <i class="fa-solid fa-arrow-up"></i>
+    </button>
+  </div>
+
+  <Header />
+
   <slot />
+
+  <Footer />
+</div>
