a67a6173629a24dcb468245dea1a1d7a9f171ad8
Stefano Bisio <s.b.milano@gmail.com>
2023-10-08 16:00:39 +0100

    style button and move event listener up

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,12 +24,15 @@
         ? "opacity-full pointer-events-auto"
         : "opacity-0 pointer-events-none")}
   >
-    <button>
-      <i class="fa-solid fa-arrow-up" on:click={goTop} />
+    <button
+      on:click={goTop}
+      class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer"
+    >
+      <i class="fa-solid fa-arrow-up grid place-items-center aspect-square" />
     </button>
   </div>
 
-  <Header />
+  <Header {scrollY} />
 
   <slot />
 
