5cc2cc17aa8f8c9c04bfe985c5e7ce11d2a47031
Stefano Bisio <s.b.milano@gmail.com>
2023-10-08 15:47:15 +0100

    add goTop button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,24 @@
   import Header from '../components/Header.svelte';
   import Footer from '../components/Footer.svelte';
 
+  let scrollY = 0;
+  let innerHeight = 0;
+  let innerWidth = 0;
+
+  function goTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
 </script>
 
 <div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
   <div class="">
     <button>
-      <i class="fa-solid fa-arrow-up"></i>
+      <i class="fa-solid fa-arrow-up" on:click={goTop}
+      ></i>
     </button>
   </div>
 
@@ -18,3 +30,5 @@
 
   <Footer />
 </div>
+
+<svelte:window bind:scrollY={scrollY} bind:innerHeight bind:innerWidth />
