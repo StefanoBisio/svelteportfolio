225576360599ad9fc565d8b2bb1828130a9f77f7
Stefano Bisio <s.b.milano@gmail.com>
2023-10-10 22:20:23 +0100

    add about section and data

---
+++ src/benefits.js
@@ -0,0 +1,18 @@
+export let benefits = [
+    {
+        metric: "10x",
+        name: "a self taught developer",
+        description:
+            "lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus. lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus",
+    },
+    {
+        name: "a product design & UX fanatic",
+        description:
+        "lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus. lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus",
+    },
+    {
+        name: "an excellent communicator",
+        description:
+        "lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus. lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus",
+    },
+];
