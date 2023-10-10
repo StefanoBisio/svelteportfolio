b0c576d037bceb43caff85bab17ffa950797984b
Stefano Bisio <s.b.milano@gmail.com>
2023-10-10 22:07:55 +0100

    add projects section and data

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,7 +1,10 @@
 <script>
+    import Project from "./Project.svelte";
+    import { projects } from "../projects.js";
 </script>
 
 <main class="flex flex-col flex-1 p-4">
+    
     <section
         id="introPage"
         class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
@@ -20,7 +23,7 @@
             <a
                 href="#"
                 target="_blank"
-                class="blueShadow mx-auto lg:mx-0 lg:inline-block lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+                class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
             >
                 <div
                     class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20
@@ -37,4 +40,29 @@
             />
         </div>
     </section>
+
+    <section class="py-20 lg:py-32 flex flex-col gap-24" id="projects">
+        <div class="flex flex-col gap-2 text-center">
+            <h6 class="text-large sm:text-xl md:text-2xl">
+                A few of my creative endeavors.
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                Curious to <span class="poppins text-violet-400">see</span> my work?
+            </h3>
+        </div>
+        <a
+            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            target="_blank"
+            class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+        >
+            <i class="fa-regular fa-circle-play" />
+            <p>Watch the video</p>
+        </a>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12">
+            {#each projects as project}
+                <Project {project} />
+            {/each}
+        </div>
+    </section>
+    
 </main>
---
+++ src/projects.js
@@ -0,0 +1,37 @@
+export let projects = [
+    {
+        link: "https://www.google.com",
+        name: "project name 1",
+        icon: "fa-solid fa-cart-shopping",
+        description:
+            "lorem ipsum dolor sit amet, <strong class='text-violet-400' >Next.js, Commerce.js, Stripe & Node.js + Express.js!</strong>, nisl eget aliquam ultricies, quam nisl aliquet nunc, quis ultricies nisl nisl nec nisl.",
+    },
+    {
+        link: "https://www.google.com",
+        name: "project name 2",
+        icon: "fa-solid fa-list-check",
+        description:
+            "lorem ipsum dolor sit amet, <strong class='text-violet-400' >Next.js, Commerce.js, Stripe & Node.js + Express.js!</strong>, nisl eget aliquam ultricies, quam nisl aliquet nunc, quis ultricies nisl nisl nec nisl.",
+    },
+    {
+        link: "https://www.google.com",
+        name: "project name 3",
+        icon: "fa-solid fa-diagram-project",
+        description:
+            "lorem ipsum dolor sit amet, <strong class='text-violet-400' >Next.js, Commerce.js, Stripe & Node.js + Express.js!</strong>, nisl eget aliquam ultricies, quam nisl aliquet nunc, quis ultricies nisl nisl nec nisl.",
+    },
+    {
+        link: "https://www.google.com",
+        name: "project name 4",
+        icon: "fa-solid fa-diagram-project",
+        description:
+            "lorem ipsum dolor sit amet, <strong class='text-violet-400' >Next.js, Commerce.js, Stripe & Node.js + Express.js!</strong>, nisl eget aliquam ultricies, quam nisl aliquet nunc, quis ultricies nisl nisl nec nisl.",
+    },
+    {
+        link: "https://www.google.com",
+        name: "project name 5",
+        icon: "fa-solid fa-diagram-project",
+        description:
+            "lorem ipsum dolor sit amet, <strong class='text-violet-400' >Next.js, Commerce.js, Stripe & Node.js + Express.js!</strong>, nisl eget aliquam ultricies, quam nisl aliquet nunc, quis ultricies nisl nisl nec nisl.",
+    },
+];
