d0213dcadd5bd498164fd020010477250e3ecfa5
Stefano Bisio <s.b.milano@gmail.com>
2023-10-10 22:09:51 +0100

    fix css of back to top button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,9 +26,9 @@
   >
     <button
       on:click={goTop}
-      class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer"
+      class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square"
     >
-      <i class="fa-solid fa-arrow-up grid place-items-center aspect-square" />
+      <i class="fa-solid fa-arrow-up grid place-items-center" />
     </button>
   </div>
 
